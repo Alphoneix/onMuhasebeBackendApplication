d73ed7f8c394fca10683951dae497f59a824c26c
Alptekin Yalçın <alptekin.yalc.n@gmail.com>
2025-08-22 17:07:01 +0300

    fix: added GetMapping anotation

--- src/main/java/com/yalcin/alptekin/onmuhasebe/IncomeController.java
+++ src/main/java/com/yalcin/alptekin/onmuhasebe/IncomeController.java
@@ -1,12 +1,24 @@
 package com.yalcin.alptekin.onmuhasebe;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/income")
 public class IncomeController {
 
+    private final IncomeService incomeService;
 
+    public IncomeController(IncomeService incomeService) {
+        this.incomeService = incomeService;
+    }
+
+    @GetMapping
+    public List<Income> getAllIncomes() {
+        return incomeService.getAllIncomes();
+    }
 
 }

66a0cdadba3866fbf52737edc42c3bfb70353fbf
Alptekin Yalçın <alptekin.yalc.n@gmail.com>
2025-08-22 17:04:28 +0300

    feat: getter function for all incomes

---
+++ src/test/java/com/yalcin/alptekin/onmuhasebe/OnMuhasebeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yalcin.alptekin.onmuhasebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnMuhasebeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

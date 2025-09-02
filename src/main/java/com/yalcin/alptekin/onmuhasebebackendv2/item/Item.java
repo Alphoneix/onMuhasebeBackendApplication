7c33e55a807e1835fee3d11f9b887c16910f5208
Alptekin Yalçın <alptekin.yalc.n@gmail.com>
2025-09-02 03:00:42 +0300

    feat: create Item entity for customer and product tracking

---
+++ src/main/java/com/yalcin/alptekin/onmuhasebebackendv2/item/Item.java
@@ -0,0 +1,36 @@
+package com.yalcin.alptekin.onmuhasebebackendv2.item;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "product_name")
+    private String productName;
+
+    @Column(name = "customer_name")
+    private String customerName;
+
+    @Column(name = "start_date")
+    private LocalDate startDate;
+
+    @Column(name = "duration_unit") // "MONTH", "YEAR"
+    private String durationUnit;
+
+    @Column(name = "duration_value")
+    private Integer durationValue;
+
+    @Column(name = "payment_frequency_unit") // "ONCE", "MONTH", "YEAR"
+    private String paymentFrequencyUnit;
+
+    @Column(name = "payment_frequency_value")
+    private Integer paymentFrequencyValue;
+}
